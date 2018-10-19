49c2bb6d37cd7214595e95ebb03097bf351da010
clementef <clementef2000@gmail.com>
2018-10-18 19:47:26 -0700

    connected mlab database

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,6 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'A^JXuBxf0ow'
-connect("", host='')
+connect("classcatalog", host='mongodb://admin:1superadmin@ds039950.mlab.com:39950/classcatalog')
 
 from .routes import *
