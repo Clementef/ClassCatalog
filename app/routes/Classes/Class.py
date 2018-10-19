7b2f53633752be4c3c30918dfa686bd67ffaaf59
Jackton Levy <jacktonbrigs@gmail.com>
2018-10-18 20:07:45 -0700

    base commit

--- app/routes/Classes/Class.py
+++ app/routes/Classes/Class.py
@@ -1,8 +1,10 @@
 from mongoengine import Document, ListField, EmbeddedDocument, EmbeddedDocumentField, ReferenceField
 from .Teacher import Teacher
 
+
 class Period(EmbeddedDocument):
     teachers = ReferenceField(Teacher)
+
 
 class Class(Document):
     periods = ListField(EmbeddedDocumentField(Period))
