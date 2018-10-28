114714dd1672471c9ce06cb95405fe8e4f0d1515
Jackton Levy <jacktonbrigs@gmail.com>
2018-10-27 19:50:01 -0700

    Slot class created

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'A^JXuBxf0ow'
-# connect("classcatalog", host='mongodb://admin:1superadmin@ds039950.mlab.com:39950/classcatalog')
-connect("classtest", host='mongodb://admin:password@ds045679.mlab.com:45679/classtest')
+connect("classcatalog", host='mongodb://admin:1superadmin@ds039950.mlab.com:39950/classcatalog')
+
 
 from .routes import *
