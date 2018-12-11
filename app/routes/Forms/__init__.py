0021424131910bd614dea8e5f78c5304fa46fcc5
Jackton Levy <jacktonbrigs@gmail.com>
2018-12-10 20:17:09 -0800

    Login added

--- app/routes/Forms/__init__.py
+++ app/routes/Forms/__init__.py
@@ -1,2 +1,3 @@
 from .CourseForm import *
 from .TeacherForm import TeacherForm
+from .LoginForm import Login
--- app/routes/index.py
+++ app/routes/index.py
@@ -1,8 +1,21 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app.routes import app
+from .Forms import Login
 
 
 @app.route("/")
 def index():
 
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    form = Login(request.form)
+
+    if request.method == "POST" and form.validate():
+
+        return redirect("/login")
+
+    return render_template("login.html", form=form)
