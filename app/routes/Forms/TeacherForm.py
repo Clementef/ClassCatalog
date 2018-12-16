7386d536b72e8bd68524984b8feb2449baab74c6
Jackton Levy <jacktonbrigs@gmail.com>
2018-12-16 13:28:56 -0800

    minor tweak

--- app/routes/Forms/TeacherForm.py
+++ app/routes/Forms/TeacherForm.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+
 class TeacherForm(FlaskForm):
     name = StringField("Enter your name:")
     submit = SubmitField()
--- app/test.py
+++
@@ -1,8 +0,0 @@
-import pandas
-
-import mongoengine
-
-db = pandas.read_csv("classes.csv")
-
-for i in db["Course title"]:
-    print(i)
