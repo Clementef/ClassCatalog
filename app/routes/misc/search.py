d4cb27b3ebf46dccf9ada2b7963ab628b71f434f
Jackton Levy <jacktonbrigs@gmail.com>
2018-12-16 19:43:54 -0800

    fixed nav bug

--- app/routes/misc/search.py
+++ app/routes/misc/search.py
@@ -3,33 +3,30 @@ from app.routes.Classes import Teacher, Course
 
 def searchteachers(find):
 
+    ls = []
     find = find.lower()
-
     find = find.split()
 
     for i in find:
-
         for j in Teacher.objects:
-
             if i in j.name.lower():
+                ls.append(j.name)
 
-                print(j.name)
+    return ls
 
 
 def searchcourses(find):
 
+    ls = []
     find = find.lower()
-
     find = find.split()
 
     for i in find:
-
         for j in Course.objects:
-
             if i in j.name.lower():
+                ls.append(j.name)
 
-                print(j.name)
+    return ls
 
 
-searchcourses("ap")
 
