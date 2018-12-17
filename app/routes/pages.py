c590aded4fd7bf663364d338012e6459cca6e69b
clementef <clementef2000@gmail.com>
2018-12-16 21:43:20 -0800

    Added to teacherpage and coursepage

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -1,27 +1,28 @@
 from flask import render_template
-from .Classes import Teacher, Course, Room
+from .Classes import Teacher, Course, Room, Section
 from app.routes import app
 
 
 @app.route("/teacher/<name>")
 def teacherpage(name):
+    # sections = []
+    # for section in Section.objects:
+    #     if section.teacher.name.replace(" ", "") == name:
+    #         sections.append(section)
 
     for i in Teacher.objects:
 
         if name == i.name.replace(" ", ""):
-
-            return render_template("teacherpage.html", teacher=i)
+            return render_template("teacherpage.html", teacher=i, sections=sections)
 
     return
 
 
 @app.route("/course/<name>")
 def coursepage(name):
-
     for i in Course.objects:
 
         if name == i.name.replace(" ", "").replace("/", ""):
-
             return render_template("coursepage.html", course=i)
 
     return "Course Not Found"
