06fc1f1962b2fb59e3416883b1c7bfb96b7d0dc9
Jackton Levy <jacktonbriggs@gmail.com>
2018-12-18 13:48:29 -0800

    period sort

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -13,7 +13,7 @@ def teacherpage(name):
     for i in Teacher.objects:
 
         if name == i.name.replace(" ", ""):
-            return render_template("teacherpage.html", teacher=i, sections=Section.objects(teacher=i))
+            return render_template("teacherpage.html", teacher=i, sections=Section.objects(teacher=i).order_by("period"))
 
     return "Teacher not found"
 
@@ -23,7 +23,7 @@ def coursepage(name):
     for i in Course.objects:
 
         if name == i.name.replace(" ", "").replace("/", ""):
-            return render_template("coursepage.html", course=i, sections=Section.objects(course=i))
+            return render_template("coursepage.html", course=i, sections=Section.objects(course=i).order_by("period"))
 
     return "Course not found"
 
@@ -35,6 +35,6 @@ def roompage(name):
 
         if name in i.location:
 
-            return render_template("roompage.html", room=i, sections=Section.objects(room=i))
+            return render_template("roompage.html", room=i, sections=Section.objects(room=i).order_by("period"))
 
     return "Room not found"
